fc6605e28fd9c7a0d1d5845b1302140a5b5e24f3
angelo <angelowilliams1015@gmail.com>
2023-08-05 19:31:32 +0200

    update

--- frontend/src/pages/Doctors/Doctors.jsx
+++ frontend/src/pages/Doctors/Doctors.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import DoctorList from "../../components/Doctors/DoctorList";
 
 const Doctors = () => {
-  return <div>Doctors</div>;
+  return (
+    <section>
+      <div className="container">
+        <div className="xl:w-[470px] mx-auto ">
+          <h2 className="heading text-center">Our great doctors</h2>
+          <p className="text_para text-center">
+            World-class care for everyone. Our health System offers <br />{" "}
+            unmatched, expert health care.
+          </p>
+        </div>
+
+        <DoctorList />
+      </div>
+    </section>
+  );
 };
 
 export default Doctors;
--- frontend/src/pages/Services.jsx
+++ frontend/src/pages/Services.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import ServiceList from "../components/services/ServiceList";
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <section>
+      <div className="container">
+        <div className="xl:w-[470px] mx-auto ">
+          <h2 className="heading text-center">Our medical services</h2>
+          <p className="text_para text-center">
+            World-class care for everyone. Our health System offers unmatched,
+            expert health care.
+          </p>
+        </div>
+
+        <ServiceList />
+      </div>
+    </section>
+  );
 };
 
 export default Services;
